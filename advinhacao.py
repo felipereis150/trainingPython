07fd372e26f9dff5a168ced5c04d72d14ef4351f
Felipe Reis <felipereis150@gmail.com>
2021-07-21 23:40:59 -0300

    added while loop

--- advinhacao.py
+++ advinhacao.py
@@ -1,22 +1,27 @@
 import random
 
-print("Bem vindo jogo de advinhação")
+print("Welcome to the guessing game!")
 
 secret_number = random.randint(1,99)
+total_attempts = 3
+remaining_attempts = total_attempts
 print(secret_number)
 
-guess = int(input("Try to guess the number in a range of 1 to 99: "))
-print ("you entered the number", guess)
+while(remaining_attempts >0):
+    print("Attempt", remaining_attempts, "of", total_attempts)
+    guess = int(input("Try to guess the number in a range of 1 to 99: "))
+    print ("you entered the number", guess)
 
-guessed = guess == secret_number
-bigger = guess < secret_number
-smaller = guess > secret_number
+    guessed = guess == secret_number
+    bigger = guess < secret_number
+    smaller = guess > secret_number
 
-if(guessed):
-    print("Correct! You guessed the number")
-elif(bigger):
-    print("try a bigger number")
-else:
-    smaller
-
-print("Game over. Run the program to play again")
+    if(guessed):
+        print("Correct! You guessed the number")
+    elif(bigger):
+        print("try a bigger number")
+    else:
+        smaller
+    remaining_attempts -= 1
+    print(total_attempts)
+print("Game Over")
